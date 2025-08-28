da37270528287f5ed0edd3e4550e7e5dbff55b3a
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-08-28 11:51:30 +0000

    feat: add cli

--- src/osync/__init__.py
+++ src/osync/__init__.py
@@ -1,2 +1,12 @@
-def main() -> None:
-    print("Hello from osync!")
+from . import cli
+
+
+def main():
+    args = cli.main()
+    if args.push:
+        print(f"Pushing to {args.path}")
+    elif args.pull:
+        print(f"Pulling from {args.path}")
+
+    if args.force:
+        print("Operation will be forced.")
