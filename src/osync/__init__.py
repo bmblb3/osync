5f75e392f119de1edf51e080d8bab5587db6f018
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-08-28 18:15:10 +0000

    feat: working app

--- src/osync/__init__.py
+++ src/osync/__init__.py
@@ -1,12 +1,28 @@
 from . import cli
+from .command_builder import RsyncCommand
+from .file_patterns_builder import FilePatterns
+from .findup import findup
+from .path_resolver import PathResolver
 
 
 def main():
     args = cli.main()
-    if args.push:
-        print(f"Pushing to {args.path}")
-    elif args.pull:
-        print(f"Pulling from {args.path}")
+    pattern_config = findup("osync.yaml")
+    file_patterns = FilePatterns.from_yml(pattern_config)
 
-    if args.force:
-        print("Operation will be forced.")
+    path_resolver = PathResolver()
+    if args.push:
+        source = path_resolver.to_local(args.path)
+        dest = path_resolver.to_remote(args.path)
+    else:
+        source = path_resolver.to_remote(args.path)
+        dest = path_resolver.to_local(args.path)
+    cmd = RsyncCommand(
+        push=args.push,
+        pull=args.pull,
+        force=args.force,
+        file_patterns=file_patterns,
+        source=source.as_posix(),
+        dest=dest.parent.as_posix() + "/.",
+    )
+    print(cmd.args)
