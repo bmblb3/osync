da63eefc86baf535055ea90f1b478a8eb67a3100
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-08-29 09:35:22 +0000

    refactor: simplify file_patterns code, tests passing

--- src/osync/file_pattern.py
+++ src/osync/file_pattern.py
@@ -25,31 +25,18 @@ class FilePatterns(list[FilePattern]):
     def from_yml(cls, file: str):
         with open(file, "r") as f:
             data = yaml.safe_load(f)  # pyright:ignore[reportAny]
-        if not isinstance(data, list):
-            raise ValueError(
-                "The patterns YAML file must contain a list of pattern dicts"
-            )
-        if not all(isinstance(item, dict) for item in data):  # pyright:ignore[reportUnknownVariableType]
-            raise ValueError(
-                "The patterns YAML file must contain a list of pattern dicts"
-            )
-        return cls.from_file_patterns(cast(filepatterns_type, data))
+        data = cast(filepatterns_type, data)
+        return cls.from_file_patterns(data)
 
     @classmethod
     def from_file_patterns(cls, patterns_dicts: filepatterns_type):
-        items: list[FilePattern] = []
-        for pdict in patterns_dicts:
-            desc = cast(str, pdict["desc"])
-            patterns = cast(list[str], pdict["patterns"])
-            push = cast(bool, pdict["push"])
-            pull = cast(bool, pdict["pull"])
-
-            assert isinstance(desc, str)
-            assert isinstance(patterns, list)
-            assert isinstance(push, bool)
-            assert isinstance(pull, bool)
-
-            items.append(
-                FilePattern(desc=desc, patterns=patterns, push=push, pull=pull)
+        items = [
+            FilePattern(
+                desc=cast(str, pdict["desc"]),
+                patterns=cast(list[str], pdict["patterns"]),
+                push=cast(bool, pdict["push"]),
+                pull=cast(bool, pdict["pull"]),
             )
+            for pdict in patterns_dicts
+        ]
         return cls(items)
